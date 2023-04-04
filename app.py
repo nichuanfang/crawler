772de4f623bf8113f9d76d0b708141b96159b936
nichuanfang <f18326186224@gmail.com>
2023-04-04 15:33:18 +0800

    app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 
 import json
 
@@ -13,30 +13,31 @@ app = Flask(__name__)
 
 app.debug = True
 
-@app.route(rule='/get',methods=['get'])
+
+@app.route(rule='/get', methods=['get'])
 def get_http():
     return request.base_url
 
 
-@app.route(rule='/post',methods=['post'])
+@app.route(rule='/post', methods=['post'])
 def post_http():
     """测试http调用
 
     Returns:
         _type_: json格式化数据
-    """    
+    """
     if not request.data:
-        return 'Fail' 
+        return 'Fail'
     params = request.data.decode('utf-8')
     # 获取到POST过来的数据，因为我这里传过来的数据需要转换一下编码。根据晶具体情况而定
     params = json.loads(params)
     print(params)
-    return jsonify(params) 
+    return jsonify(params)
 
 
-def query_tickets(begin:str,end:str,offset:int,mode:int,type:int,seats:int,train_no:str):
+def query_tickets(begin: str, end: str, **kwargs):
     """查询余票(默认当天 普通模式 有票 高铁动车)
-
+    
     Args:
         begin (str):出发地
 
@@ -51,42 +52,64 @@ def query_tickets(begin:str,end:str,offset:int,mode:int,type:int,seats:int,train
         seats (int):座位类型 0:不过滤类型(默认) 1: 一等座  2: 二等座 3: 无座
 
         train_no(str):默认不限制 限制车次
-    """ 
-    # opts.fs = from_station
-    # opts.ts = to_station
-    # if date:
-    #     opts.date = date
-    # if seats:
-    #     opts.seats = seats
-    # if train_no:
-    #     opts.train_no = train_no
-    # opts.zmode = zmode
-    # opts.zzmode = zzmode
-    # opts.gcd = gcd
-    # opts.ktz = ktz
-    # opts.remaining = remaining
+    """
+    data = {}
+    data['fs'] = begin
+    data['ts'] = end
+    data['date'] = (datetime.datetime.now() +
+                    datetime.timedelta(days=offset)).strftime('%Y-%m-%d')
+    if kwargs['seats']:
+        match kwargs['seats']:
+            case 1:
+                data['seats'] = '一等座'
+            case 2:
+                data['seats'] = '二等座'
+            case 3:
+                data['seats'] = '无座'
+            case other:
+                pass
+    if kwargs['train_no']:
+        data['train_no'] = kwargs['train_no']
+    if kwargs['mode']:
+        match kwargs['mode']:
+            case 1:
+                data['zmode'] = True
+                data['zzmode'] = False
+            case 2:
+                data['zmode'] = False
+                data['zzmode'] = True
+            case other:
+                data['zmode'] = False
+                data['zzmode'] = False
+        pass
+    if kwargs['type']:
+        match kwargs['type']:
+            case 1:
+                data['gcd'] = True
+                data['ktz'] = False
+            case 2:
+                data['gcd'] = False
+                data['ktz'] = True
+            case other:
+                data['gcd'] = False
+                data['ktz'] = False
+    data['remaining'] = True
+    # todo 加载代理文件和cdn
     # if proxies_file:
     #     opts.proxies_file = proxies_file
     # if stations_file:
     #     opts.stations_file = stations_file
     # if cdn_file:
-    #     opts.cdn_file = cdn_file    
-
-
-    data = {}
-
-
-
-    date = (datetime.datetime.now()+datetime.timedelta(days=offset)).strftime('%Y-%m-%d')
-    
+    #     opts.cdn_file = cdn_file
 
     tt = TrainTable()
-    tt.update_trains(data)
+    tt.update_trains(data)  # type: ignore
     tt.echo()
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     # 开启http服务
-    app.run('127.0.0.1',port=5000)
-    
+    # query_tickets('杭州','武汉')
+    # app.run('127.0.0.1', port=5000)
+    query_tickets('杭州','五哈尼')
+    pass
