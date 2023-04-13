71fda5a866873fea19880fb84f6e213d0a0bd7f4
chuanfang <f18326186224@gmail.com>
2023-04-13 11:27:04 +0800

    fix bugs

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 /usr/local/bin/pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r ./requirements.txt
-/usr/local/bin/python setup.py install
-/usr/local/bin/python app.py
 bash stop.sh
 bash start.sh
 docker restart nginx
