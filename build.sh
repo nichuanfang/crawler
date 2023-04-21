887d97541ed6c3fd20febd53b0a5d86b3e6f9e9b
倪传方 <1290274972@qq.com>
2023-04-21 21:13:36 +0800

    add bash log

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # 重新构建镜像 (修改版本)
 docker rm -vf crawler
+echo '开始构建镜像...'
 docker build -t registry.cn-hangzhou.aliyuncs.com/jayzhou/crawler:latest --build-arg CRAWLER_VERSION=v1.0.0 /root/docker/dockerfile_work/crawler
+echo '镜像构建完成!'
 # 推送到阿里云镜像仓库
+echo '推送镜像到阿里云镜像仓库...'
 docker push registry.cn-hangzhou.aliyuncs.com/jayzhou/crawler:latest
+echo '推送镜像到阿里云镜像仓库完成!'
 # 删除本地镜像
+echo '开始清理本地残留镜像...'
 docker rmi -f $(docker images | grep crawler | awk '{print $3}')
 docker rmi -f ubuntu:latest
+echo '本地残留镜像清理完成!'
 # 执行docker-compose
+echo 'docker-compose up...'
 docker-compose -f /root/docker/docker-compose/docker-compose.yml up -d
+echo 'docker-compose up completed!'
