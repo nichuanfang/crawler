c0340d23f7bffcac4028c0bc93eff537508cc6c5
倪传方 <1290274972@qq.com>
2023-05-17 00:15:20 +0800

    trigger deploy

--- my_selenium/my_selenium.py
+++ my_selenium/my_selenium.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 import logging
 
 # 日志设置
-LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s" 
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
 
